bf2c7fca878123e54a01b6e7cc6d0076e697fe7b
smaillex26 <alami.ism26@gmail.com>
2026-02-25 11:08:05 +0100

    dernier commit

--- cypress/e2e/smart-tag.cy.js
+++ cypress/e2e/smart-tag.cy.js
@@ -3,30 +3,30 @@ describe('Smart Tag Component Tests', () => {
     cy.visit('../../src/smart-tag.html');
   });
 
-  // Test 1: Tester l'apparition du tag au survol du bouton "See more"
+  // Test 1
   it('should show the tag when hovering over the "See more" button', () => {
-    // Vérifier que le tag n'est pas visible initialement
+    
     cy.get('[x-show="isActive"]').should('not.be.visible');
     
-    // Survoler le bouton "See more"
+    
     cy.contains('div', 'See more').trigger('mouseover');
     
-    // Vérifier que le tag est maintenant visible
+    
     cy.get('[x-show="isActive"]').should('be.visible');
   });
 
-  // Test 2: Tester la disparition du tag à la sortie du survol du bouton "See more"
+  // Test 2
   it('should hide the tag when moving the mouse away from the "See more" button', () => {
-    // Survoler le bouton "See more"
+    
     cy.contains('div', 'See more').trigger('mouseover');
     
-    // Vérifier que le tag est visible
+   
     cy.get('[x-show="isActive"]').should('be.visible');
     
-    // Cliquer ailleurs pour déclencher mouseover.away
+    
     cy.get('body').click(0, 0);
     
-    // Vérifier que le tag est maintenant caché
+    
     cy.get('[x-show="isActive"]').should('not.be.visible');
   });
 });
--- cypress/e2e/todolist.cy.js
+++ cypress/e2e/todolist.cy.js
@@ -5,38 +5,38 @@ describe('TodoList Component Tests', () => {
 
   // Test: Ajouter quatre todos puis supprimer la deuxième et vérifier qu'il ne reste que 3 todos
   it('should add four todos, delete the second one, and verify only 3 todos remain', () => {
-    // Ajouter la première todo
+    
     cy.get('input[placeholder="What needs to be done?"]').type('Todo 1');
     cy.get('button[type="submit"]').click();
     
-    // Ajouter la deuxième todo
+    
     cy.get('input[placeholder="What needs to be done?"]').type('Todo 2');
     cy.get('button[type="submit"]').click();
     
-    // Ajouter la troisième todo
+    
     cy.get('input[placeholder="What needs to be done?"]').type('Todo 3');
     cy.get('button[type="submit"]').click();
     
-    // Ajouter la quatrième todo
+    
     cy.get('input[placeholder="What needs to be done?"]').type('Todo 4');
     cy.get('button[type="submit"]').click();
     
-    // Vérifier que 4 todos sont présentes
+    
     cy.get('[x-text="todo.content"]').should('have.length', 4);
     
-    // Vérifier le compteur affiche 4
+    
     cy.contains('Nombre de tâche(s) :').parent().should('contain', '4');
     
-    // Supprimer la deuxième todo en cliquant sur la croix associée
+  
     cy.get('button[type="button"]').eq(1).click();
     
-    // Vérifier qu'il ne reste que 3 todos
+   
     cy.get('[x-text="todo.content"]').should('have.length', 3);
     
-    // Vérifier le compteur affiche 3
+  
     cy.contains('Nombre de tâche(s) :').parent().should('contain', '3');
     
-    // Vérifier que les bonnes todos restent (1, 3, 4)
+    
     cy.get('[x-text="todo.content"]').eq(0).should('contain', 'Todo 1');
     cy.get('[x-text="todo.content"]').eq(1).should('contain', 'Todo 3');
     cy.get('[x-text="todo.content"]').eq(2).should('contain', 'Todo 4');
