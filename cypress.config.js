7de53f629a8e02782410b5529a3c3a3c53374f2d
smaillex26 <alami.ism26@gmail.com>
2026-02-25 10:22:56 +0100

    Fix: Correction de la configuration Cypress pour CI/CD

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,14 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-  baseUrl: "file:///",
-  
   e2e: {
+    baseUrl: "file:///",
+    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+    supportFile: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
-
   video: true,
   screenshotOnRunFailure: true,
 });
